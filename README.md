65df8adcd2875c69fa533d0de11f9e5e46f03f82
duratm <duratmathias@gmail.com>
2024-11-04 10:53:19 +0100

    feat(elixir): initialize elixir sfu server project

---
+++ README.md
@@ -0,0 +1,17 @@
+# Webrtclixir
+
+To start your Phoenix server:
+
+  * Run `mix setup` to install and setup dependencies
+  * Start Phoenix endpoint with `mix phx.server` or inside IEx with `iex -S mix phx.server`
+
+Now you can visit [`localhost:4000`](http://localhost:4000) from your browser.
+
+
+## This project is inspired by the following sources.
+
+  * Official website: https://www.phoenixframework.org/
+  * Guides: https://hexdocs.pm/phoenix/overview.html
+  * Docs: https://hexdocs.pm/phoenix
+  * Forum: https://elixirforum.com/c/phoenix-forum
+  * Source: https://github.com/phoenixframework/phoenix
---
+++ lib/webrtclixir/application.ex
@@ -0,0 +1,37 @@
+defmodule Webrtclixir.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      WebrtclixirWeb.Telemetry,
+      {DNSCluster, query: Application.get_env(:webrtclixir, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Webrtclixir.PubSub},
+      # Start a worker by calling: Webrtclixir.Worker.start_link(arg)
+      # {Webrtclixir.Worker, arg},
+      # Start to serve requests, typically the last entry
+      WebrtclixirWeb.Endpoint,
+      WebrtclixirWeb.Presence,
+      Webrtclixir.PeerSupervisor,
+      Webrtclixir.Room,
+      {Registry, name: Webrtclixir.PeerRegistry, keys: :unique}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Webrtclixir.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  @impl true
+  def config_change(changed, _new, removed) do
+    WebrtclixirWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end

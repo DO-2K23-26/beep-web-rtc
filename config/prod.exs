1a07a72312c99a846ef3fffca5a248f10ede4f09
duratm <duratmathias@gmail.com>
2025-02-14 11:55:44 +0100

    change auth

--- config/prod.exs
+++ config/prod.exs
@@ -21,3 +21,5 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+config :webrtclixir, :auth,
+       url: "http://localhost:3333"
